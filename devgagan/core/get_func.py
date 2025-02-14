34b60d38fb8be4d46a67437a4d3dea4d2fac8ae4
Sushilgithubbot <sushilgithubbot@gmail.com>
2025-02-14 18:25:35 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -888,7 +888,7 @@ async def handle_large_file(file, sender, edit, caption):
         if freecheck == 1:
             reply_markup = InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("💎 Get Premium to Forward", url="https://t.me/kingofpatal")]
+                    [InlineKeyboardButton("💎 Get Premium to Forward", url="https://t.me/personalSKbot")]
                 ]
             )
             await app.copy_message(
